f7d8217d73d80a1c6332c660a956cb1bc899189c
Aurélien LITRA <aurelien.litra@gmail.com>
2024-05-07 10:57:28 +0200

    upload application.py

--- application.py
+++ application.py
@@ -7,20 +7,35 @@ from Statistiques import Statistiques
 class Application:
     def __init__(self, master):
         self.master = master
-        master.title("Navigation Bar")
+        self.pages = {}
 
-        #Méthode pour prendre les paramètre d'écran de l'ordinateur et prend 80%
-        screen_width = master.winfo_screenwidth()
-        screen_height = master.winfo_screenheight()
+    def Popup(self):
+        popup = tk.Toplevel(self.master)
+        popup.title("Bienvenue au jeu du Cookie Clicker")
+        popup.geometry("500x200")
+        self.label_rules = tk.Label(popup, text="Bonjour et bienvenue sur le jeu du Cookie Clicker !! ")
+        self.label_rules.pack(padx=25)
+        self.button_rules = tk.Button(popup, text="ok", anchor="center", command=lambda: [popup.destroy(), self.affichage_principale()], height=3, width=50, bg='#E2BFB3')
+        self.button_rules.pack(anchor="center")
+
+    def affichage_principale(self):
+
+        # Méthode pour prendre les paramètre d'écran de l'ordinateur et prend 80%
+        screen_width = self.master.winfo_screenwidth()
+        screen_height = self.master.winfo_screenheight()
         window_width = int(screen_width * 0.8)
         window_height = int(screen_height * 0.8)
-        master.geometry(f"{window_width}x{window_height}+{int((screen_width - window_width) / 2)}+{int((screen_height - window_height) / 2)}")
+        self.master.geometry(f"{window_width}x{window_height}+{int((screen_width - window_width) / 2)}+{int((screen_height - window_height) / 2)}")
 
-        self.pages = {}
+
+        self.master.deiconify()
         self.create_navigation_bar()
         self.create_pages()
+        self.master.title("Navigation Bar")
+
 
     def create_navigation_bar(self):
+
         navigation_frame = tk.Frame(self.master, bg="#E2BFB3", relief=tk.SUNKEN)
         navigation_frame.pack(side="top", fill="x")
 
@@ -44,17 +59,19 @@ class Application:
     # ICI POUR DEFINIR LES PAGES
 
     #=*=
+
+        self.statistiques_page = self.pages["Statistiques"]
+        self.stat_instance = Statistiques(self.statistiques_page)
+
         self.cookie_page = self.pages["Cookie"]
-        self.cookie_instance = Cookie(self.cookie_page)
+        self.cookie_instance = Cookie(self.cookie_page, self.stat_instance)
 
         self.boutique_page = self.pages["Boutique"]
         self.boutique = Upgrade(self.boutique_page, self.cookie_instance)
 
         self.minijeu_page = self.pages["Mini-Jeu"]
-        self.minijeu = Minijeu(self.minijeu_page)
+        self.minijeu = Minijeu(self.minijeu_page, self.cookie_instance, self.stat_instance)
 
-        self.statistiques_page = self.pages["Statistiques"]
-        self.statistiques = Statistiques(self.statistiques_page)
     #=*=
 
         self.show_page("Cookie")
