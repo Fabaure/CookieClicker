ef7aeda94ef1e01ef8307ba633ae71603aeaefa5
Aurélien LITRA <aurelien.litra@gmail.com>
2024-05-15 15:47:43 +0200

    Upload main.py

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ from tkinter import PhotoImage
 from application import Application
 from minijeu1 import Minijeu1
 
-
-
 class CookieClickerApp:
     def __init__(self, master):
         self.master = master
         self.master.withdraw()
         self.application = Application(master)
-        self.application.Popup()
+        self.application.MainMenu()
 
 
 if __name__ == "__main__":
